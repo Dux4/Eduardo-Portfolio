443d943b7b85fb4735ecd97be0f19749963a7aea
Eduardo <eduardopassos55551@gmail.com>
2023-09-13 21:45:50 -0300

    Adicionei minha foto | Links pessoais

---
+++ app.js
@@ -0,0 +1,5 @@
+function Erro() {
+    var enviarMsg = document.getElementById("EnviarMensagem")
+    enviarMsg = "[Erro] Ainda estamos em construção... Para falar comigo use os links na seção Sobre mim, ou mande um e-mail."
+    alert (enviarMsg)
+}
